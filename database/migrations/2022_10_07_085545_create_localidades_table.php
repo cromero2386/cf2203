b5e13a2ef9d9122d2d41fd7e09c6075cd0aba5cb
cromero2386 <cromero2386@gmail.com>
2022-10-07 09:05:14 -0300

    first commit

---
+++ database/migrations/2022_10_07_085545_create_localidades_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocalidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('localidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            //Clave foranea
+            $table->unsignedBigInteger('provincia_id');
+            // Linea donde el indico con que clave primaria de que tabla se relaciona
+            $table->foreign('provincia_id')->references('id')->on('provincias');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('localidades');
+    }
+}
