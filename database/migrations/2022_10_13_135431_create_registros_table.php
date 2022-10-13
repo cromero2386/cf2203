c8d5bb5dfb1b11a8fd65c6123eb7380193f908a8
cromero2386 <cromero2386@gmail.com>
2022-10-13 14:02:14 -0300

    agrega modelo de persona vacio y agrega completo registro

---
+++ database/migrations/2022_10_13_135431_create_registros_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('fechaHoraIngreso');
+            $table->dateTime('fechaHoraEgreso');
+            //Clave foranea
+            $table->unsignedBigInteger('persona_id');
+            // Linea donde el indico a persona_id con que clave primaria de que tabla se relaciona
+            $table->foreign('persona_id')->references('id')->on('personas');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registros');
+    }
+};
