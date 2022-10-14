7099d7acc3da1b42d4d037c39e87906e1c8a8916
cromero2386 <cromero2386@gmail.com>
2022-10-14 18:43:27 -0300

    Agrega metodos de areas

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -17,9 +17,16 @@ class AreaController extends Controller
         //SELECT id, nombre FROM areas;
         $areas = Area::select('id', 'nombre')->get();
 
+        $data = $areas->map(function ($area) {
+            return [
+                'area_id' => $area->id,
+                'area_nombre' => $area->nombre,
+            ];
+        });
+
         return response()->json([
-            'mensaje' => 'Listado de areas',
-            'data' => $areas,
+            'mensaje' => 'Listado de areas disponibles',
+            'data' => $data,
         ]);
     }
 
@@ -41,7 +48,21 @@ class AreaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Validación
+        $request->validate([
+            'nombre' => ['required'],
+            'interno' => ['required', 'integer', 'max:4'],
+        ]);
+
+        $area = Area::create([
+            'nombre' => $request['nombre'],
+            'interno' => $request['interno'],
+        ]);
+
+        return response()->json([
+            'mensaje' => 'Se Agrego Correctamente el area',
+            'data' => $area,
+        ]);
     }
 
     /**
@@ -52,10 +73,16 @@ class AreaController extends Controller
      */
     public function show(int $id)
     {
-        $area = Area::select('id', 'nombre')->where('id', $id)->get();
+        $data = Area::findOrFail($id);
+
+        $area = [
+            'area_id' => $data->id,
+            'area_nombre' => $data->nombre,
+            'area_interno' => $data->interno,
+        ];
 
         return response()->json([
-            'mensaje' => 'El area es:',
+            'mensaje' => 'Datos del area solicitada',
             'data' => $area,
         ]);
     }
@@ -78,9 +105,22 @@ class AreaController extends Controller
      * @param  \App\Models\Area  $area
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Area $area)
+    public function update(Request $request, int $id)
     {
-        //
+        $request->validate([
+            'nombre' => ['required'],
+            'interno' => ['required', 'integer', 'max:4'],
+        ]);
+
+        $area = Area::findOrFail($id);
+        $area->nombre = $request['nombre'];
+        $area->interno = $request['interno'];
+        $area->save();
+
+        return response()->json([
+            'mensaje' => 'Datos del area modificada',
+            'data' => $area,
+        ]);
     }
 
     /**
